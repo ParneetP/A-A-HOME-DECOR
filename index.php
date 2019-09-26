939d610e7428539959a9310391db3b66e925a80d
ParneetP <ParneetP@users.noreply.github.com>
2019-09-26 21:07:52 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,116 @@
 <?php include('header.php'); ?>
+<<<<<<< Updated upstream
 
 	Body Of The Home Page<br>
 	Slides Of The Home Page<br>
 	Team Information<br>
 	Content Of The Home Page
 	<!-- //contact -->
+=======
+<!-- works -->
+<div class="works" id="works">
+<div class="container">
+	<div class="w3-agileits-works-grids">
+		<div class="col-md-4 works-left">
+			<div class="w3ls-heading works-heading">
+				<h3>Our Works</h3>
+				<h5>A&A Home decor is focusing on home decor products</h5>
+			</div>
+			<div class="works-info">
+				<p>Home decoration refers to the art and science of making an internal and external space more attractive and functionally useful for its residents. It used for decorating your home with various designer elements such as color, style of furniture, floor design etc.	</div>
+		</div>
+		<div class="col-md-8 work-right">
+			<div class="gallery-grids">
+				<div class="gallery-top-grids">
+					<div class="col-sm-3 gallery-grids-left">
+						<div class="gallery-grid">
+								<img src="tools/images/a1.jpg" alt="">
+								<div class="captn">
+									<h4> A&A </h4>
+									<p>Home Decor Australia</p>
+								</div>
+						</div>
+					</div>
+					<div class="col-sm-3 gallery-grids-left">
+						<div class="gallery-grid">
+									<img src="tools/images/a2.jpg" alt="">
+								<div class="captn">
+									<h4> A&A </h4>
+									<p>Home Decor Australia</p>
+								</div>
+						</div>
+					</div>
+					<div class="col-sm-3 gallery-grids-left">
+						<div class="gallery-grid">
+								<img src="tools/images/a3.jpg" alt="">
+								<div class="captn">
+									<h4> A&A </h4>
+									<p>Home Decor Australia</p>
+								</div>
+							
+						</div>
+					</div>
+					<div class="col-sm-3 gallery-grids-left">
+						<div class="gallery-grid">
+									<img src="tools/images/a4.jpg" alt="">
+								<div class="captn">
+									<h4> A&A </h4>
+									<p>Home Decor Australia</p>
+								</div>
+						</div>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="gallery-top-grids">
+					<div class="col-sm-3 gallery-grids-left">
+						<div class="gallery-grid">
+									<img src="tools/images/a5.jpg" alt="">
+								<div class="captn">
+									<h4> A&A </h4>
+									<p>Home Decor Australia</p>
+								</div>
+						</div>
+					</div>
+					<div class="col-sm-3 gallery-grids-left">
+						<div class="gallery-grid">
+								<img src="tools/images/a6.jpg" alt="">
+								<div class="captn">
+									<h4> A&A </h4>
+									<p>Home Decor Australia</p>
+								</div>
+							</div>
+					</div>
+					<div class="col-sm-3 gallery-grids-left">
+						<div class="gallery-grid">
+								<img src="tools/images/a7.jpg" alt="">
+								<div class="captn">
+									<h4> A&A </h4>
+									<p>Home Decor Australia</p>
+								</div>
+							</div>
+					</div>
+					<div class="col-sm-3 gallery-grids-left">
+						<div class="gallery-grid">
+							<a class="example-image-link" href="tools/images/a1.jpg" data-lightbox="example-set" data-title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae cursus ligula">
+								<img src="tools/images/a1.jpg" alt="">
+								<div class="captn">
+									<h4> A&A </h4>
+									<p>Home Decor Australia</p>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="clearfix"> </div>
+				<script src="tools/js/lightbox-plus-jquery.min.js"> </script>
+			</div>
+		</div>
+		<div class="clearfix"> </div>
+	</div>
+</div>
+</div>
+	<!-- //works -->
+	
+>>>>>>> Stashed changes
 <?php include('footer.php'); ?>
