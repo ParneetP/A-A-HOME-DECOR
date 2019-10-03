bff41f63148cefe950ad030613a01cc01845a0d1
ParneetP <46293246+ParneetP@users.noreply.github.com>
2019-10-03 10:35:09 +1000

    Update login.php

--- login.php
+++ login.php
@@ -23,15 +23,7 @@
 					<input type="password" name="password" placeholder="Password" required="">
 				</div>
 			</div>
-			
-			<input type="submit" value="Submit">
-			<input type="reset" value="Clear Form">
-			<div class="clear"></div>
-		</form>
-		<!--// Form starts here -->
-	</div>
-
-	<script type="text/javascript" src="tools/log/js/jquery-2.2.3.min.js"></script>
+			g/js/jquery-2.2.3.min.js"></script>
 	<!-- Calendar Js -->
 	<script src="tools/log/js/jquery-ui.js"></script>
 	<script>
