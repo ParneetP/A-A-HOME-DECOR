8b79f08d08cda9ebd73ba2416e05b8643d37fc08
RishiramPaudel <54305913+RishiramPaudel@users.noreply.github.com>
2019-10-03 11:17:31 +1000

    Connection_last_for_Alpha_release

--- connection.php
+++ connection.php
@@ -5,7 +5,7 @@ $password = "";
 $hostname = "localhost"; 
 
 //connection to the database
-$db_handle = mysqli_connect($hostname,$username,$password) or die("Unable to connect to MySQL");
+$db_handle = mysqli_connect($hostname,$username,$password) or die("Unable to connect to MYSql database");
 $selected = mysqli_select_db($db_handle,"homedecore") or die("Could not select database");
 error_reporting(0);
 ?>
