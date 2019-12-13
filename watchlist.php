aa3d5d188ff48abe8bc09ad18ac2085557693f6d
RishiramPaudel <54305913+RishiramPaudel@users.noreply.github.com>
2019-12-13 12:33:31 +1000

    Add files via upload
    watchlist

---
+++ watchlist.php
@@ -0,0 +1,51 @@
+<?php include('header.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Wishlist of <?php echo $_SESSION["user_Firstname"];?></h2>
+  <p>List of products you wish us to add in A&A homedecor</p>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Product Name</th>
+        <th>Product Category</th>
+        <th>Product Description</th>
+      </tr>
+    </thead>
+    <tbody>
+	<?php
+      $user='root';
+      $pass='';
+      $db='homedecor';
+      $conn=mysqli_connect('localhost',$user, $pass, $db);
+
+      $ProductSQL = "select * from watchlist where user_id ='{$_SESSION['user_id']}'";
+      $ProductResult = mysqli_query($conn, $ProductSQL);?>
+  <?php while ($row = mysqli_fetch_assoc($ProductResult)) { ?>
+      <tr class="success">
+        <td><?php echo $row['product_name']; ?></td>
+        <td><?php echo $row['product_category']; ?></td>
+        <td><?php echo $row['product_description']; ?></td>
+      </tr>
+  <?php } ?>
+
+    </tbody>
+  </table>
+</div>
+<a href="add_watchlist.php">Add Wishlist Item</a>
+</body>
+</html>
+
+
+<!-- //leave-comments -->
+	<?php include('footer.php'); ?>
