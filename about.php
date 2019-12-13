9e67a8afbe6ceaacad72738c3fac35938c1cf06b
Sharnjit <46259942+Sharnjit@users.noreply.github.com>
2019-12-13 12:09:16 +1000

    about added

---
+++ about.php
@@ -0,0 +1,97 @@
+<?php include('header.php'); ?>
+	<!-- about -->
+	<div class="about" id="about">
+		<div class="container">
+			<div class="w3ls-heading">
+				<h2>About Us</h2>
+				<p>We believe that decorating your home should be fun</p>
+			</div>
+			<div class="agileits-about-grids">
+				<div class="col-md-6 agileits-about-grid">
+					<p>When we’re not at our desks writing the content you love, we’re discovering the next big things by meeting with top interior designers, connecting with the heads of your favorite brands, attending leading design events, and joining forces with industry authorities—all to bring you original news, breakout trends, practical tips, and inspiration within the home and design space.</p>
+				</div>
+				<div class="col-md-6 agileits-about-grid">
+					<p>Our product reviews are independent and based on research and product testing — if you visit links within our content, we may receive commissions from your purchases, but we never receive any compensation or consideration for the content of our recommendations.</p>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="agileinfo-about-slide">
+				<div id="owl-demo" class="owl-carousel owl-theme">
+					<div class="item">
+						<div class="about-grid-info">
+							<img src="tools/images/a1.jpg" alt="" />
+							<div class="about-grid-caption">
+								<h4>Ideas By</h4>
+								<p>A&A Homedecor</p>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="about-grid-info">
+							<img src="tools/images/a2.jpg" alt="" />
+							<div class="about-grid-caption"> 
+                            <h4>Ideas By</h4>
+								<p>A&A Homedecor</p>
+							</div>
+						</div>	
+					</div>
+					<div class="item">
+						<div class="about-grid-info">
+							<img src="tools/images/a3.jpg" alt="" />
+							<div class="about-grid-caption"> 
+                            <h4>Ideas By</h4>
+								<p>A&A Homedecor</p>
+							</div>
+						</div>	
+					</div>
+					<div class="item">
+						<div class="about-grid-info">
+							<img src="tools/images/a4.jpg" alt="" />
+							<div class="about-grid-caption"> 
+                            <h4>Ideas By</h4>
+								<p>A&A Homedecor</p>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="about-grid-info">
+							<img src="tools/images/a5.jpg" alt="" />
+							<div class="about-grid-caption"> 
+							<h4>Ideas By</h4>
+								<p>A&A Homedecor</p>
+							</div>
+						</div>	
+					</div>
+					<div class="item">
+						<div class="about-grid-info">
+							<img src="tools/images/a6.jpg" alt="" />
+							<div class="about-grid-caption"> 
+                            <h4>Ideas By</h4>
+								<p>A&A Homedecor</p>
+							</div>
+						</div>	
+					</div>
+					<div class="item">
+						<div class="about-grid-info">
+							<img src="tools/images/a7.jpg" alt="" />
+							<div class="about-grid-caption"> 
+                            <h4>Ideas By</h4>
+								<p>A&A Homedecor</p>
+							</div>
+						</div>	
+					</div>
+					<div class="item">
+						<div class="about-grid-info">
+							<img src="tools/images/a1.jpg" alt="" />
+							<div class="about-grid-caption">
+							<h4>Ideas By</h4>
+								<p>A&A Homedecor</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //about -->
+	<?php include('footer.php'); ?>
