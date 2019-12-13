bee256ddfea71f46fa8bccd007bc764264cc5687
ParneetP <46293246+ParneetP@users.noreply.github.com>
2019-12-13 12:30:56 +1000

    Update homedecore.sql

--- homedecore.sql
+++ homedecore.sql
@@ -106,7 +106,7 @@ CREATE TABLE `user` (
   `user_contact` varchar(12) NOT NULL,
   `user_address` varchar(100) NOT NULL,
   `user_email` varchar(30) NOT NULL,
-  `user_password` varchar(30) NOT NULL
+  `user_password` varchar(30) 
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
