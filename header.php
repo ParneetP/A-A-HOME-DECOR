6b2f3c71d597626293ad6866dcf36612efe332d1
RishiramPaudel <54305913+RishiramPaudel@users.noreply.github.com>
2020-01-03 13:07:52 +1000

    Add files via upload

--- header.php
+++ header.php
@@ -1,23 +1,33 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
-<?php include('connection.php'); ?>
-
 <html lang="en">
 <head>
 <title>A&A HomeDecor</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="tools/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
 <link rel="stylesheet" href="tools/css/style.css" type="text/css" media="all" />
-
 <link rel="stylesheet" href="tools/css/owl.carousel.css" type="text/css" media="all">
 <link href="tools/css/owl.theme.css" rel="stylesheet">
 <link type="text/css" rel="stylesheet" href="tools/css/cm-overlay.css" />
-
 <link href="tools/css/font-awesome.css" rel="stylesheet"> 
 <link href="//fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
-
-<script src="tools/tools/js/jquery-1.11.1.min.js"></script>
-<script src="tools/tools/js/bootstrap.js"></script>
-
+<script src="tools/js/jquery-1.11.1.min.js"></script>
+<script src="tools/js/bootstrap.js"></script>
+<script>
+$(document).ready(function() { 
+	$("#owl-demo").owlCarousel({
+		autoPlay: 3000, //Set AutoPlay to 3 seconds
+		autoPlay:true,
+		items : 3,
+		itemsDesktop : [640,5],
+		itemsDesktopSmall : [414,4]
+	});
+}); 
+</script>
+<!-- light-box -->
+<!-- //light-box -->
 </head>
 <body>
 	<!-- banner -->
@@ -26,14 +36,14 @@
 			<div class="container">
 				<div class="agileinfo-social-grids">
 					<ul>
-						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="https://www.facebook.com/aahomedecor47/"><i class="fa fa-facebook"></i></a></li>
 						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
 						<li><a href="#"><i class="fa fa-rss"></i></a></li>
-						<li><a href="#"><i class="fa fa-vk"></i></a></li>
+						<li><a href ="#"><i class="fa fa-vk"></i></a></li>
 					</ul>
 				</div>
 				<div class="w3layouts-banner-right">
-					<h1><a href="tools/index.html">A&A HomeDecor</a></h1>
+				<a href="index.php" ><img src="tools/images/aahdaus.png.png" style="height:110px;width:150px;margin-right:-65px;" alt="" /></a>
 				</div>
 				<div class="clearfix"> </div>
 			</div>
@@ -43,25 +53,25 @@
 				<section class="slider">
 					<div class="flexslider">
 						<ul class="slides">
-							<li>	
+							<li>
 								<div class="w3layouts-banner-info">
 									<h4>A WIDE
-									<span>HOMEDECOR</span>
-									 PRODUCTS</h4>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at gravida libero. Quisque mauris urna, feugiat sed nisi et, dignissim tincidunt neque.</p>
+									<span>SELECTION</span>
+									OF PRODUCTS</h4>
+									<p>Browse A&A Home Decor wode selection of products by catagory </p>
 									<div class="w3-button">
-										<a href="tools/single.html" class="btn btn-1 btn-1b">Read More</a>
+										<a href="#" class="btn btn-1 btn-1b">Read More</a>
 									</div>
 								</div>
 							</li>
-							<li>	
+							<li>
 								<div class="w3layouts-banner-info">
 									<h4>THE BEST
 									<span>HOMEDECOR</span>
-									PRODUCTS1</h4>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at gravida libero. Quisque mauris urna, feugiat sed nisi et, dignissim tincidunt neque.</p>
+									SERVICES</h4>
+									<p>Get Membership of A&A Home decor and take advantage of services </p>
 									<div class="w3-button">
-										<a href="tools/single.html" class="btn btn-1 btn-1b">Read More</a>
+										<a href="#" class="btn btn-1 btn-1b">Read More</a>
 									</div>
 								</div>
 							</li>
@@ -70,7 +80,7 @@
 				</section>
 				<!-- flexSlider -->
 				<link rel="stylesheet" href="tools/css/flexslider.css" type="text/css" media="screen" property="" />
-				<script defer src="tools/tools/js/jquery.flexslider.js"></script>
+				<script defer src="tools/js/jquery.flexslider.js"></script>
 				<script type="text/javascript">
 					$(window).load(function(){
 						$('.flexslider').flexslider({
@@ -95,17 +105,33 @@
 								<span class="icon-bar"></span>
 							</button>
 						</div>
-						<!-- Collect the nav links, forms, and other content for toggling -->
 						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 							<ul class="nav navbar-nav">
-								<li><a class="active" href="index.php">Home</a></li>
-								<li><a href="#" class="scroll">About</a></li>
-								<li><a href="#" class="scroll">Products</a></li>
-								<li><a href="#" class="scroll">Contact</a></li>
+								<li><a  href="index.php" class="scroll">Home</a></li>
+								<li><a href="about.php" class="scroll">About</a></li>
+								<li><a href="gallery.php" class="scroll">Gallery</a></li>
+								<li><a href="products_bedsheets.php" class="scroll">Products</a></li>
+								<li><a href="#contact" class="scroll">Contact us</a></li>
+								<?php if(isset($_SESSION['user_Firstname'])){ ?>
+									<li><a href="watchlist.php" class="scroll">Wishlist</a></li>
+									<li><a href="codes/logout.php" class="scroll">Logout</a></li>
+								<?php } else{ ?>
+									<li><a href="login.php" class="scroll">Login</a></li>
+									<li><a href="signup.php" class="scroll">Signup</a></li>
+								<?php }?>
 							</ul>
-							<div class="clearfix"> </div>							
+							<div class="clearfix"> </div>			
 						</div>	
 					</nav>		
 				</div>
 		</div>
 	</div>
+	<script>
+    $(function(){
+        $('a').each(function(){
+            if ($(this).prop('href') == window.location.href) {
+                $(this).addClass('active'); $(this).parents('li').addClass('active');
+            }
+        });
+    });
+</script>
