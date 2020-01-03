bb3ebf437af9fac1da740a6bcd574ca7afd497fe
ParneetP <46293246+ParneetP@users.noreply.github.com>
2020-01-04 09:31:21 +1000

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -21,6 +21,7 @@
                     </div> <!-- text-right / end -->
                 </a>
             </div> 
+            
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
                 <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b2.jpg">
                     <img class="img-responsive" alt="" src="tools/images/b2.jpg" />
@@ -87,215 +88,61 @@
                     <div class='text-right'>
                   
                     </div> <!-- text-right / end -->
-                </a<?php include('header.php'); ?>
-<style>
-.logo{
-  padding:10px;
-  text-decoration:none;
-  color:#E64980;
-  border:1px solid #E64980;
-  font-weight:1000;
-}
-
-.logo span{
-  position:relative;
-  display:inline-block;
-  
-  font-family:fontAwesome;
-  width:20px;
-  height:20px;
-  color:black;
-  transition:0.5s;
-}
-.logo span:after{
-  position:absolute;
-  content:'\00bb';
-  top:5px;
-  left:20px;
-  color:white;
-  opacity:0;
-  transition:0.5s;
-  
-}
-
-.logo:hover{
-  padding-left:5px;
-  padding-right:20px;
-  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
-  background:#E64980;
-  color:white;
-  transition-duration: 0.5s;
-  }
-
-.logo:hover span:after{
-  opacity:1;
-  left:0;
-  padding-left:5px;
-}
-
-select{
-  margin-right:5px;
- padding:10px;
-  cursor:pointer;
-}
-
-input{
-  
-  padding:10px;
-  outline:none;
-  border:none;
-  border-radius:10px;
-  }
-
-.srch{
-  padding:10px;
-  cursor:pointer;
-  color:black;
-}
-
-.srch:after{
-  content:'\f002';
-  font-family:fontAwesome;
-}
-
-.wrapper{
-  border:1px solid black;
-  display:inline-flex;
-  padding:0px;
-  border-radius:10px;
-}
-
-
-@media(max-width:457px)
-{
-  select{
-    display:none;
-   }
-  
-  .menu-toggle{
-    display:inline-flex;
-  color:black;
-  width:40px;
-    float:right;
-    font-size:40px;
-  height:40px;
-    text-align:center;
-}
-  
-  .menu-toggle:after{
-    content:'\f0c9';
-    font-family:fontAwesome;
-  }
-
-  .menu-toggle.active:after{
-    content:'\f00d';
-    font-family:fontAwesome;
-  }
-
-  
-}
-
-@media(max-width:356px){
-  .wrapper{
-    display:none;
-  }
-  
-}
-
-.button {
-  color: #fff;
-  text-decoration: none;
-  background-color: #000;
-  padding: 5px;
-  border-radius: 0px;
-  font-weight: 800;
-  font-size: 20px;
-  font-family: sans-serif;
-  text-transform: uppercase;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  display: inline-block;
-}
-</style>
-
-<a href="#" class="logo">Pillow<span></span></a> 
-<select name="forma" onchange="location = this.value;">
-<option value="products_pillow.php">Pillow Cases</option>
-<option value="products_bedsheets.php">Bedsheets</option>
-<option value="products_curtains.php">Curtains</option>
-
-</select>
-
-<select class="">
-  <option value=""></option>
-  <option value=""></option>
-  <option value=""></option>
-</select>
-<select class="">
-  <option value=""></option>
-  <option value=""></option>
-  
-</select>
-<div class="wrapper">
-<form method="post" action="products_search.php">
-  <input type="text" name="search" placeholder="search.." />
-  <span class="srch"></span>
-</div>
-<input type="submit" value="Search" class="button">
-</form>
-
-<?php
-      $user='root';
-      $pass='';
-      $db='homedecor';
-      $conn=mysqli_connect('localhost',$user, $pass, $db);
-      $ProductSQL = "select * from products where productcatagory='pillow' ";
-      $ProductResult = mysqli_query($conn, $ProductSQL);?>
-
-  <?php while ($row = mysqli_fetch_assoc($ProductResult)) { ?>
-
-<div class="menu-toggle"></div>
-<div class="blog" id="blog">
-		<div class="container">
-			<div class="w3ls-heading">
-				<h3>Our Pillow Products</h3>
-				<p>Browse our Pillow products, get best prices</p>
-			</div>
-			<div class="w3-agile-blog-grids">
-				<div class="w3-agile-blog-grid">
-					<div class="col-md-5 w3-agile-blog-left">
-						<a href="single.html"><img src="tools/images/<?php echo $row['productimage']; ?>" alt="" /></a>
-					</div>
-					<div class="col-md-6 w3-agile-blog-right">
-						<div class="w3-agile-blog-right-top">
-							<div class="blog-left-left">
-								<i class="fa fa-pencil" aria-hidden="true"></i>
-							</div>
-							<div class="blog-left-right-top">
-								<h4><a href="#"><?php echo $row['productname']; ?></a></h4>
-								<p>Arrived At: <?php echo $row['arrivedat']?></p>
-							</div>
-							<div class="clearfix"> </div>
-						</div>
-						<div class="w3-agile-blog-right-info">
-							<p><?php echo $row['productdetails']; ?></a></p>
-              <p>Price: $<?php echo $row['productprice']; ?></a></p>
-							<a href="payment.php">Buy Now</a>
-						</div>
-					</div>
-					<div class="clearfix"> </div>
-				</div>
-        <?php } ?>
-			</div>
-		</div>
-	</div>
-    <?php include('footer.php'); ?>
+                </a>
             </div> 
             <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
-              
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b9.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b9.jpg" />
                     <div class='text-right'>
                         <small class='text-muted'></small>
-                    </di
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b10.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b10.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b11.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b11.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b12.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b12.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b13.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b13.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div>
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b22.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b22.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b23.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b23.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
                     </div> <!-- text-right / end -->
                 </a>
             </div>
@@ -410,10 +257,174 @@ input{
                         <small class='text-muted'></small>
                     </div> <!-- text-right / end -->
                 </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b31.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b31.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div>
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b32.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b32.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
                     </div> <!-- text-right / end -->
                 </a>
             </div> <!-- col-6 / end -->
-            <div class='col-sm-4 col-xs-6 col
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b33.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b33.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div>
+        </div> <!-- list-group / end -->
+	</div> <!-- row / end -->
+    <div class="row">
+		<div class='list-group gallery'>
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b34.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b34.jpg" />
+                    <div class='text-right'>
+                  
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> 
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b45.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b35.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b36.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b36.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b37.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b37.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b38.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b38.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b39.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b39.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div>
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b40.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b40.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b41.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b41.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div>
+        </div> <!-- list-group / end -->
+	</div>
+    <div class="row">
+		<div class='list-group gallery'>
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b42.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b42.jpg" />
+                    <div class='text-right'>
+                  
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> 
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b43.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b43.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b44.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b44.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b45.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b45.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b46.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b46.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b47.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b47.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div>
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b48.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b48.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div> <!-- col-6 / end -->
+            <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
+                <a class="thumbnail fancybox" rel="ligthbox" href="tools/images/b60.jpg">
+                    <img class="img-responsive" alt="" src="tools/images/b60.jpg" />
+                    <div class='text-right'>
+                        <small class='text-muted'></small>
+                    </div> <!-- text-right / end -->
+                </a>
+            </div>
+        </div> <!-- list-group / end -->
+	</div>
+</div> <!-- container / end -->
+<script>
+$(document).ready(function(){
+    
+    $(".fancybox").fancybox({
         openEffect: "none",
         closeEffect: "none"
     });
