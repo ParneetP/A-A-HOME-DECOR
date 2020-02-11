761e2d1de02aa7f957fa118122d52d414716213d
ParneetP <ParneetP@users.noreply.github.com>
2020-02-11 12:13:11 +1000

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 	<div class="footer" style="background:grey;">
 		<div class="container">
 			<div class="social">
@@ -94,4 +95,53 @@ $('body').noisy({
 
 
 </body>	
+=======
+	<div class="footer">
+		<div class="container">
+			<div class="social">
+				<ul>
+					<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+					<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+					<li><a href="#"><i class="fa fa-rss"></i></a></li>
+					<li><a href="#"><i class="fa fa-vk"></i></a></li>
+				</ul>
+			</div>
+			<div class="border"></div>
+			<div class="agileits-w3layouts-copyright">
+				<p>A & A Home Decor</a></p>
+			</div>
+		</div>
+	</div>
+	<script src="tools/tools/js/responsiveslides.min.js"></script>
+	
+	<script type="text/javascript">
+		/* init Jarallax */
+		$('.jarallax').jarallax({
+			speed: 0.5,
+			imgWidth: 1388,
+			imgHeight: 768
+		})
+	</script>
+
+<script type="text/javascript" src="tools/tools/js/easing.js"></script>
+	
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			*/
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+
+<script src="tools/tools/js/owl.carousel.js"></script>  
+</body>	
+>>>>>>> Stashed changes
 </html>
